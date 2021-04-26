1564dac83d40070d193025ff32283060b614ba9f
GopalTiwari7 <gopal.tiwari_cs18@gla.ac.in>
2021-04-26 22:33:04 +0530

    create ChatFeed

--- chatapp/src/App.js
+++ chatapp/src/App.js
@@ -1,2 +1,16 @@
 import {ChatEngine} from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed';
 import './App.css';
+
+const App = () =>{
+    return(
+        <ChatEngine
+            height="100vh"
+            projectID=""
+            userName=""
+            userSecret=""
+            renderChatFeed={(ChatAppProps) => <ChatFeed {...ChatAppProps} />} 
+        />
+    )
+}
+export default App;
