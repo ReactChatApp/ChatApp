081190fc0dc08dfa1c74804b8e330a4bee02f579
ayush <ayush.goyal_cs18@gla.ac.in>
2021-05-01 21:00:37 +0530

    adding props

--- chatapp/src/components/MessageForm.jsx
+++ chatapp/src/components/MessageForm.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { SendOutlined, PictureOutlined } from '@ant-design/icons';
 import { sendMessage, isTyping } from 'react-chat-engine';
 
-const MessageForm = () =>{
+const MessageForm = (props) =>{
     const [value, setValue] = useState('');
   const { chatId, creds } = props;
 
