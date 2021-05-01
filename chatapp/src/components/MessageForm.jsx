1ed9ae13ebcd120f906b74baea1a70f21ffbe37a
ayush <ayush.goyal_cs18@gla.ac.in>
2021-05-01 20:59:59 +0530

    importing react chat engine

--- chatapp/src/components/MessageForm.jsx
+++ chatapp/src/components/MessageForm.jsx
@@ -1,4 +1,31 @@
+import { useState } from 'react';
+import { SendOutlined, PictureOutlined } from '@ant-design/icons';
+import { sendMessage, isTyping } from 'react-chat-engine';
+
 const MessageForm = () =>{
+    const [value, setValue] = useState('');
+  const { chatId, creds } = props;
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+
+    isTyping(props, chatId);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const text = value.trim();
+
+    if (text.length > 0) {
+      sendMessage(creds, chatId, { text });
+    }
+
+    setValue('');
+  };
+
+  const handleUpload = (event) => {
+    sendMessage(creds, chatId, { files: event.target.files, text: '' });
+  };
     return(
         <form className="message-form" onSubmit={handleSubmit}>
       <input
