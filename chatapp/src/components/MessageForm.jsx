321beac49d4dd1b123b0bcd755cf029cc1cb561d
ayush <ayush.goyal_cs18@gla.ac.in>
2021-05-01 20:57:58 +0530

    creating message form

--- chatapp/src/components/MessageForm.jsx
+++ chatapp/src/components/MessageForm.jsx
@@ -1,8 +1,29 @@
 const MessageForm = () =>{
     return(
-        <div>
-            MessageForm
-        </div>
+        <form className="message-form" onSubmit={handleSubmit}>
+      <input
+        className="message-input"
+        placeholder="Send a message..."
+        value={value}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+      />
+      <label htmlFor="upload-button">
+        <span className="image-button">
+          <PictureOutlined className="picture-icon" />
+        </span>
+      </label>
+      <input
+        type="file"
+        multiple={false}
+        id="upload-button"
+        style={{ display: 'none' }}
+        onChange={handleUpload.bind(this)}
+      />
+      <button type="submit" className="send-button">
+        <SendOutlined className="send-icon" />
+      </button>
+    </form>
     )
 }
 export default MessageForm;
