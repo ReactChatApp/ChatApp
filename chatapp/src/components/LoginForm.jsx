b8c4dc8409b657464bb1f61e5534d02044da47e5
ayush <ayush.goyal_cs18@gla.ac.in>
2021-05-02 21:42:30 +0530

    creating login form

--- chatapp/src/components/LoginForm.jsx
+++ chatapp/src/components/LoginForm.jsx
@@ -3,5 +3,25 @@ import axios from 'axios';
 const LoginForm = () => {
     const [username, setUsername]=usestate('');
     const [password, setPassword]=usestate('');
-
+    const handleSubmit = (e) => {
+            e.preventDefault();
+    }
+    return (
+        <div className="wrapper">
+          <div className="form">
+            <h1 className="title">Chat Application</h1>
+            <form onSubmit={handleSubmit}>
+              <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="input" placeholder="Username" required />
+              <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="input" placeholder="Password" required />
+              <div align="center">
+                <button type="submit" className="button">
+                  <span>Start chatting</span>
+                </button>
+              </div>
+            </form>
+            <h1>{error}</h1>
+          </div>
+        </div>
+    
+      );
 }
