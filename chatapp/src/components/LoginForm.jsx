9c07975b5f23fb231db52dafe1900423d3d594fc
ayush <ayush.goyal_cs18@gla.ac.in>
2021-05-03 21:10:08 +0530

    end commit

--- chatapp/src/components/LoginForm.jsx
+++ chatapp/src/components/LoginForm.jsx
@@ -1,20 +1,18 @@
 import { useState } from 'react';
 import axios from 'axios';
 const LoginForm = () => {
-    const [username, setUsername]=usestate('');
-    const [password, setPassword]=usestate('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
     const [error, setError] = useState('');
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
             e.preventDefault();
             const authObject = { 'Project-ID': "fdfdf85f-b689-4e7a-8b30-94eb155efd1a", 'User-Name': username, 'User-Secret': password }
 
-    }
     try {
         await axios.get('https://api.chatengine.io/chats', { headers: authObject });
   
         localStorage.setItem('username', username);
         localStorage.setItem('password', password);
-  
         window.location.reload();
         setError('');
       } catch (err) {
